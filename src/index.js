34f0b7591b632da6a79dc991ddcc51884ac23487
Marc Dos Santos <mdossantosdev@gmail.com>
2019-04-03 14:39:21 +0200

    Add GlobalStyles component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import GlobalStyles from './styles/globals';
 
 ReactDOM.render(
   <div>
+    <GlobalStyles />
     <App />
   </div>,
   document.getElementById('root')
